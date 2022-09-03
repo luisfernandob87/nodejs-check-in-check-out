a67b13734840454a12882bcdf343784c13c97b85
luisfernandob87 <luisfernandob87@gmail.com>
2022-09-02 21:33:20 -0600

    add catch non-existing endpoints

--- app.js
+++ app.js
@@ -7,4 +7,11 @@ app.use(express.json());
 
 app.use('/api/v1/registrations', registrationRouter);
 
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 'error',
+    message: `${req.method} ${req.url} does not exist in our server`,
+  });
+});
+
 module.exports = { app };
